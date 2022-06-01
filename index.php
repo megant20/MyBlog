b6b03cbfa86405c2f663401a69fc0d7139198d9b
megant20 <60585671+megant20@users.noreply.github.com>
2022-06-01 14:38:00 -0500

    Update index.php brainstorming
    Brainstorming the initial design of the blog

--- index.php
+++ index.php
@@ -11,7 +11,52 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <!--background image here, replace the link inside url() parentheses to change-->
+  <div style = "background: url('https://images.pexels.com/photos/3828753/pexels-photo-3828753.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') fixed no-repeat center;" class = "my-3 p-1 container-fluid shadow-lg card">
+    <!--navbar links-->
+    <ul style = "padding: 0; margin: 0; list-style-type: none;" class = "row">
+      <li class = "col-xs-3 col-md-3"><a href="#" class="btn btn-outline-secondary" target = "_blank">Text</a></li>
+      <li class = "col-xs-3 col-md-3"><a href="#" class="btn btn-outline-secondary" target = "_blank">Text</a></li>
+      <li class = "col-xs-3 col-md-3"><a href="#" class="btn btn-outline-secondary" target = "_blank">Text</a></li>
+      <li class = "col-xs-3 col-md-3"><a href="https://toyhou.se/gotaise" class="btn btn-outline-secondary" target = "_blank">Credit</a></li>
+      <!--you can remove this and add credit elsewhere, just appreciate that there's some credit somewhere-->
+
+    </ul>
+  </div>
+  <!--other background image-->
+  <div class = "shadow-lg rounded card container-fluid" style = "background: url(https://images.pexels.com/photos/3828753/pexels-photo-3828753.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940) fixed no-repeat center;">
+    <div class = "row">
+      <div class = "my-1 p-2 col-xs-4 col-md-4">
+        <!--pfp-->
+        <h1 style = "color: white; text-align: center; font-variant: small-caps;">NAME</h1>
+        <img class = "rounded" style = "border: 1px solid black; max-width: 150px;" src = "https://images.pexels.com/photos/2672520/pexels-photo-2672520.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+        <p style = "text-align: center; color: white;"><i>small text</i></p>
+      </div>
+      <div class = "p-2 my-1 col-xs-8 col-md-8">
+        <p style = "color: white; text-align: center; font-variant: small-caps;">pronouns</p>
+        <!--replace text in between p tags to write your about-->
+        <p style = "border: 2px solid black; max-height: 150px; background-color: white; text-align: center; " class = "rounded overflow-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <ul class = "mx-auto row" style = "list-style-type: none;">
+          <li class = "mx-auto my-3 col">
+            <a class = "mx-auto" href = "discord link here" target = "_blank"><i style = "color: white;" class="fa-fw fab fa-discord fa-3x" aria-hidden="true"></i></a>
+          </li>
+          <li class = "mx-auto my-3 col">
+            <a class = "mx-auto" href = "insta link here" target = "_blank"><i style = "color: white;" class="fa-fw fab fa-instagram fa-3x" aria-hidden="true"></i></a>
+          </li>
+          <li class = "mx-auto my-3 col">
+            <a class = "mx-auto" href = "DA link here" target = "_blank"><i style = "color: white;" class="fa-fw fab fa-deviantart fa-3x" aria-hidden="true"></i></a>
+          </li>
+
+
+      </ul>
+
+      </div>
+  </div>
+
+</div>
+
+
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
